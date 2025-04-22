6d440a0dbe6811241d2ae50cacb8daf59c905422
User <User@WIN-1STBJGFL3G0>
2025-04-22 15:54:10 +0500

    Добавлено обновление строки в таблице Типы аниме

--- AppTitlesAnime/FormListTypes.cs
+++ AppTitlesAnime/FormListTypes.cs
@@ -59,9 +59,9 @@ namespace AppTitlesAnime
         private void BtnAddType_Click(object sender, EventArgs e)
         {
             FormAddType formAddType = new();
-            DialogResult result = formAddType.ShowDialog(this); 
-            
-            if(result == DialogResult.Cancel) 
+            DialogResult result = formAddType.ShowDialog(this);
+
+            if (result == DialogResult.Cancel)
                 return;
 
             Type type = new Type();
@@ -71,6 +71,35 @@ namespace AppTitlesAnime
             db.SaveChanges();
 
             MessageBox.Show("Новый объект добавлен");
+
+            this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o => o.TypeName).ToList();
+        }
+
+        private void BtnUpdateType_Click(object sender, EventArgs e)
+        {
+            if( dataGridViewTypes.SelectedRows.Count == 0)
+                return;
+
+            int index = dataGridViewTypes.SelectedRows[0].Index;
+            short id = 0;
+            bool converted = Int16.TryParse(dataGridViewTypes[0, index].Value.ToString(), out id);
+            if(!converted )
+                return;
+
+            Type type = db.Types.Find(id);
+            FormAddType formAddType = new();
+            formAddType.textBoxTypeName.Text = type.TypeName;
+
+            DialogResult result = formAddType.ShowDialog( this );
+
+            if(result == DialogResult.Cancel) 
+                return;
+
+            type.TypeName=formAddType.textBoxTypeName.Text;
+            db.Types.Update(type);
+            db.SaveChanges();
+
+            MessageBox.Show("Объект изменен");
 
             this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o => o.TypeName).ToList();
         }
