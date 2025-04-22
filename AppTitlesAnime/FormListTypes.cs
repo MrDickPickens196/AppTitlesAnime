b784acb2e7b65ebbaa0f8b5018b651dd8960840d
User <User@WIN-1STBJGFL3G0>
2025-04-22 16:01:25 +0500

    Добавлено удаление строки из таблицы Типы аниме

--- AppTitlesAnime/FormListTypes.cs
+++ AppTitlesAnime/FormListTypes.cs
@@ -77,29 +77,60 @@ namespace AppTitlesAnime
 
         private void BtnUpdateType_Click(object sender, EventArgs e)
         {
-            if( dataGridViewTypes.SelectedRows.Count == 0)
+            if (dataGridViewTypes.SelectedRows.Count == 0)
                 return;
 
             int index = dataGridViewTypes.SelectedRows[0].Index;
             short id = 0;
             bool converted = Int16.TryParse(dataGridViewTypes[0, index].Value.ToString(), out id);
-            if(!converted )
+            if (!converted)
                 return;
 
             Type type = db.Types.Find(id);
             FormAddType formAddType = new();
             formAddType.textBoxTypeName.Text = type.TypeName;
 
-            DialogResult result = formAddType.ShowDialog( this );
+            DialogResult result = formAddType.ShowDialog(this);
 
-            if(result == DialogResult.Cancel) 
+            if (result == DialogResult.Cancel)
                 return;
 
-            type.TypeName=formAddType.textBoxTypeName.Text;
+            type.TypeName = formAddType.textBoxTypeName.Text;
             db.Types.Update(type);
             db.SaveChanges();
 
             MessageBox.Show("Объект изменен");
+
+            this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o => o.TypeName).ToList();
+        }
+
+        private void BtnDeleteType_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewTypes.SelectedRows.Count == 0)
+                return;
+
+            DialogResult result = MessageBox.Show(
+                "Вы уверены что хотите удалить объект? \nВсе связанные данные будут удалены.",
+                "",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+                );
+
+            if ( result == DialogResult.No ) 
+                return;
+
+            int index = dataGridViewTypes.SelectedRows[0].Index;
+            short id = 0;
+            bool converted = Int16.TryParse(dataGridViewTypes[0, index].Value.ToString(), out id);
+            if (!converted)
+                return;
+
+            Type type = db.Types.Find(id);
+
+            db.Types.Remove(type);
+            db.SaveChanges();
+
+            MessageBox.Show("Объект удален");
 
             this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o => o.TypeName).ToList();
         }
